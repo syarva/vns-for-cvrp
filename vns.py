d0d4044b2278610885e3eb99f4f1165c55999fc4
syarva <46300612+syarva@users.noreply.github.com>
2020-07-17 16:57:03 +0300

    Update vns.py

--- vns.py
+++ vns.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 14 13:14:44 2018
-
-@author: Simos
-"""
-
 import twoopt
 import swap
 import relocate
@@ -24,16 +17,16 @@ def VNS(routes,duration,cost,dm,demand,capacity,kmax):
     routing = copy.deepcopy(routes)
     newroutes = vnd.VND(routing,cost,dm,demand,capacity)
     i=1
-    #print ("kkkkkkkkkkkkkkkkkk")
+    
     while time.time() < start + duration:
-        shaked = shake.SHAKE(i%kmax,newroutes,dm,demand,capacity) #anti gia i, kmax
-        #print ("oooooooooooooooooo")
+        shaked = shake.SHAKE(i%kmax,newroutes,dm,demand,capacity) 
+        
         optshaked = vnd.VND(shaked,cost,dm,demand,capacity)
         cost2 = con.COST(optshaked,dm)
         if cost2<cost1:
             cost1 = cost2
             newroutes = copy.deepcopy(optshaked)
             i=1
-            #print ("zzzzzzzzzzzzzzz")
+            
         else: i=i+1
     return newroutes
